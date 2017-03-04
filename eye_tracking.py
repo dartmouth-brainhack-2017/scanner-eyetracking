7e9749aec5cb1f6d1524f16fe9efe769d9c96a69
sbherald <sarah@sarahs-mbp-10.wireless.dartmouth.edu>
2017-03-04 10:45:42 -0500

    Added several eye tracking methods
    I experimented with several different methods for isolating the eye or
    certain parts of the eye in the image.
    
    Unsuccessful methods were: Canny edge detection (this picks up the
    projector screen reflection well), Haar cascade eye detection (too
    unreliable with default trained classifier), Sobel filtering (too much
    noise), and different types of image value thresholding.
    
    The most successful method, which I am still working on, is converting
    to HSV color space, thresholding for the hue value of the white part of
    the eye, and then using a median blue to eliminate noise around the
    eye. It’s pretty good in consistently detecting the white part of the
    eye, but it also picks some places outside the eye. I am working to
    refine this approach.

--- eye_tracking.py
+++ eye_tracking.py
@@ -1,7 +1,8 @@
 import numpy as np
 import cv2
+# from matplotlib import pyplot as plt
 
-videoFile = "SamHemiR1.mov"
+videoFile = "SamLocR3.mov"
 cap = cv2.VideoCapture(videoFile)
 
 # XML file located in opencv/data/haarcascades/ folder
@@ -11,11 +12,59 @@ while(cap.isOpened()):
     ret, frame = cap.read()
     if ret==True:
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        eyes = eye_cascade.detectMultiScale(gray)
-        for (ex,ey,ew,eh) in eyes:
-            cv2.rectangle(frame,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-            print("eye found!")
-        cv2.imshow('frame',frame)
+
+        # Canny edge detection
+        # edges = cv2.Canny(gray,200,300)
+
+        # HSV hue detection
+        hsvframe = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        height = len(hsvframe)
+        width = len(hsvframe[0])
+        for x in range(width):
+            for y in range(height):
+                value = hsvframe[y,x,0]
+                # if (x < 200 or x > 400) or (y < 200 or y > 300):
+                    # hsvframe[y,x,2] = 0
+                # else:
+                if value < 100 or value > 105:
+                    hsvframe[y,x,2] = 0
+        newframe = cv2.cvtColor(hsvframe, cv2.COLOR_HSV2BGR)
+        newframe = cv2.medianBlur(newframe,21)
+
+        # newframe = cv2.cvtColor(newframe,cv2.COLOR_BGR2GRAY)
+        # edges = cv2.Canny(newframe,300,400)
+
+        # Sobel filtering
+        # sobel = cv2.Sobel(gray,cv2.CV_64F,1,0,ksize=5)
+        # abs_sobel = np.absolute(sobel)
+        # sobel_8u = np.uint8(abs_sobel)
+
+        # Finding coordinates for stuff (I forget what this was)
+        # for i in range(len(gray[0])):
+            # gray[:][i]
+        # N = np.matrix(gray)
+        # print(np.unravel_index(N.argmin(), N.shape))
+        # print(N.argmin())
+        # for i in range(len(gray)):
+        #     print(gray[i][400])
+
+        # Haar cascade eye detection
+        # eyes = eye_cascade.detectMultiScale(gray,scaleFactor=1.5,minSize=(200,200))
+        # for (ex,ey,ew,eh) in eyes:
+        #     cv2.rectangle(frame,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+        #     # print("eye found!")
+
+        # cv2.rectangle(frame,(200,200),(400,300),(0,255,0),2)
+        # cv2.rectangle(frame,(324,254),(326,256),(0,255,0),2)
+
+        # print(hsvframe[255][325][0])
+
+        # Display results
+        cv2.imshow('frame',newframe)
+
+        # edge_mask = cv2.bitwise_not(edges)
+        # img = frame.putalpha(edge_mask)
+
         if cv2.waitKey(25) & 0xFF == ord('q'):
             break
     else:
